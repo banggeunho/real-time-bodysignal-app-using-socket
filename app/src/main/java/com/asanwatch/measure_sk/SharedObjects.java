f25229e1fd3cd92bd56e8262232df9eced234bfc
banggeunho <panggeunho@gmail.com>
2022-07-03 00:25:28 +0900

    사용되지 않는 class 제거 (send message)

--- app/src/main/java/com/asanwatch/measure_sk/SharedObjects.java
+++ app/src/main/java/com/asanwatch/measure_sk/SharedObjects.java
@@ -4,11 +4,6 @@ import java.net.Socket;
 
 
 public class SharedObjects {
-
-    public static String address = "172.30.1.60";
-    public static int port = 5001;
     public static String deviceId = DeviceInfoUtil.getDeviceBrand()+" "+DeviceInfoUtil.getDeviceId(MainActivity.ApplicationContext());
     public static boolean isWake = true;
-    public static Socket socket = null;
-
 }
